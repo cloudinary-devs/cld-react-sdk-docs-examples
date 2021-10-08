e4ca524a675ccc0f7d16aab675bb89550d106a7c
Caroline Levison <caroline.levison@cloudinary.com>
2021-10-08 12:20:00 +0100

    Add structure for keeping the javascript separate for each example

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react'
 import {AdvancedImage} from '@cloudinary/react';
-import {Cloudinary} from "@cloudinary/url-gen";
-
-// Import any actions required for transformations.
-import {fill} from "@cloudinary/url-gen/actions/resize";
+import {Quickstart} from './quickstart';
+import {FullExample} from './fullExample';
 
 function App() {
-
-  // Create a Cloudinary instance and set your cloud name.
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: 'demo'
-    }
-  });
-
-  const myImage = cld.image('sample'); 
-
-  // Resize to 250 x 250 pixels using the 'fill' crop mode.
-  myImage.resize(fill().width(250).height(250));
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <AdvancedImage cldImg={myImage} />
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_quick_start#3_see_the_result" target="_blank" rel="noopener noreferrer">Quickstart</a>
+        <AdvancedImage cldImg={Quickstart()} />
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#full_example" target="_blank" rel="noopener noreferrer">Full example</a>
+        <AdvancedImage cldImg={FullExample()} />
       </header>
     </div>
   );
---
+++ my-app/src/quickstart.js
@@ -0,0 +1,19 @@
+import {Cloudinary} from "@cloudinary/url-gen";
+import {fill} from "@cloudinary/url-gen/actions/resize";
+
+export function Quickstart() {
+
+  // Create a Cloudinary instance and set your cloud name.
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'demo'
+    }
+  });
+
+  const myImage = cld.image('sample'); 
+
+  // Resize to 250 x 250 pixels using the 'fill' crop mode.
+  myImage.resize(fill().width(250).height(250));
+
+  return myImage;
+}
