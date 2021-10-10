21a05cedded0ea691572f59526e03986cbf61975
Caroline Levison <caroline.levison@cloudinary.com>
2021-10-10 16:07:00 +0100

    Add more examples, including plugins

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import React from 'react'
-import {AdvancedImage} from '@cloudinary/react';
+import {AdvancedImage, lazyload, accessibility, responsive, placeholder} from '@cloudinary/react';
 import {Quickstart} from './quickstart';
 import {FullExample} from './fullExample';
+import {AssetInstance} from './assetInstance';
+import {Transformations} from './transformations';
 
 function App() {
 
@@ -15,6 +17,31 @@ function App() {
 
         <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#full_example" target="_blank" rel="noopener noreferrer">Full example</a>
         <AdvancedImage cldImg={FullExample()} />
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#asset_instance_configuration" target="_blank" rel="noopener noreferrer">Asset instance</a>
+        <AdvancedImage cldImg={AssetInstance()} />    
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#transformations" target="_blank" rel="noopener noreferrer">Transformations</a>
+        <AdvancedImage cldImg={Transformations()} />  
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#plugins" target="_blank" rel="noopener noreferrer">Plugins</a>
+        <AdvancedImage cldImg={Quickstart()} plugins={[lazyload(), responsive(), accessibility(), placeholder()]} />
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#lazy_loading" target="_blank" rel="noopener noreferrer">Lazy loading</a>
+        <AdvancedImage cldImg={Quickstart()} plugins={[lazyload('10px 20px 10px 30px', 0.25)]}/>
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#responsive_images" target="_blank" rel="noopener noreferrer">Responsive images</a>
+        <AdvancedImage cldImg={AssetInstance()} plugins={[responsive([800, 1000, 1400])]} />
+
+        <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#image_accessibility" target="_blank" rel="noopener noreferrer">Image accessibility</a>
+       <AdvancedImage cldImg={AssetInstance()} plugins={[accessibility('colorblind')]}/>
+
+       <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#image_placeholders" target="_blank" rel="noopener noreferrer">Image placeholders</a>
+       <AdvancedImage cldImg={Quickstart()} plugins={[placeholder('blur')]}/>
+
+       <a className="App-link" href="https://cloudinary.com/documentation/react2_integration#example_2_combine_lazy_loading_with_a_placeholder" target="_blank" rel="noopener noreferrer">Lazy load and placeholder</a>
+       <AdvancedImage cldImg={Quickstart()} plugins={[lazyload(), placeholder('predominant-color')]}/>
+  
       </header>
     </div>
   );
---
+++ my-app/src/assetInstance.js
@@ -0,0 +1,16 @@
+// Import the configuration classes.
+import URLConfig from "@cloudinary/url-gen/config/URLConfig";
+import CloudConfig from "@cloudinary/url-gen/config/CloudConfig";
+import {CloudinaryImage} from '@cloudinary/url-gen';
+
+export function AssetInstance() {
+
+    // Set the Cloud configuration and URL configuration
+    const cloudConfig = new CloudConfig({cloudName: 'demo'});
+    const urlConfig = new URLConfig({secure: true});
+
+    // Instantiate a new image passing the Cloud and URL configurations
+    const myImage = new CloudinaryImage('sample', cloudConfig, urlConfig);
+
+    return myImage;
+}
